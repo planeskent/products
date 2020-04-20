f1db4b821947ca1214a2684aa89e998af4e758f9
planeskent <31124290+planeskent@users.noreply.github.com>
2020-04-21 00:20:48 +0800

    add header

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == "q":
 		break
 	price = input('Please input the price of product:')
+	price = int(price)
 	#the most complex but comprehensive coding
 	#p = []
 	#p.append(name)
@@ -29,5 +30,13 @@ for p in products:
 for p in products:
 	print(p[0], "cost is", p[1])
 
-	
+#Open and prepare to write the item stored in program into the file "products.csv **If the file not existing, then it will create a new file
+#"with" is python special function which all code will close after leaving with
+with open("products.csv", "w", encoding='utf-8') as f:
+	f.write('product','price\n')
+	#for loop to write in each item in products
+	for p in products:
+		#Combine all str in the parameter to 1 big str, then write into f *actual writing
+		f.write(p[0] + "," + str(p[1]) + '\n')
+
 
